b3d7c0b5cee9ebab4278c612ec77bec5984de386
glitterpillz <karenhickey1010@gmail.com>
2026-05-19 17:30:32 -0400

    update app/config file

--- app/config.py
+++ app/config.py
@@ -2,9 +2,14 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    FLASK_RUN_PORT = os.environ.get('FLASK_RUN_PORT')
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    FLASK_RUN_PORT = os.environ.get("FLASK_RUN_PORT")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace('postgres://', 'postgresql://')
+
+    database_url = os.environ.get("DATABASE_URL")
+
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_ECHO = True
