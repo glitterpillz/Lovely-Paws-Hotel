b5207e2296859014fc3c7d4eda6a61224950813a
glitterpillz <karenhickey1010@gmail.com>
2026-05-19 17:38:40 -0400

    revert config file back to simpler version

--- app/config.py
+++ app/config.py
@@ -9,18 +9,7 @@ class Config:
     database_url = os.environ.get("DATABASE_URL")
 
     if database_url and database_url.startswith("postgres://"):
-        database_url = database_url.replace(
-            "postgres://",
-            "postgresql://",
-            1
-        )
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
 
     SQLALCHEMY_DATABASE_URI = database_url
-
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        "connect_args": {
-            "sslmode": "require"
-        }
-    }
-
     SQLALCHEMY_ECHO = True
